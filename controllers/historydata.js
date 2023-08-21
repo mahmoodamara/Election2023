5700b1e7c4776b26c50ab6db19c056964a977016
mahmoodamara <mahmoodamara2@gmail.com>
2023-08-22 00:28:56 +0300

    update time

--- controllers/historydata.js
+++ controllers/historydata.js
@@ -4,17 +4,41 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 router.use(bodyParser.json());
 const  db  = require('../db');
+const cron = require('node-cron');
 
-router.post('/data', (req, res) => {
-    const insertQuery = 'INSERT INTO historydata (candidates, datedata) VALUES (?, ?)';
-    const values = [cityname, datedata, JSON.stringify(arrayofnames), JSON.stringify(arrayofvotes)];
-  
-    db.query(insertQuery, values, (err, result) => {
-      if (err) {
-        return res.json(err);
-      }
-      return res.json({ message: 'Data added successfully' });
-    });
+cron.schedule('7 0 * * *', () => {
+  const insertQuery = 'INSERT INTO historydata (candidatesid, candidatesname, candidatesvotes, datedata, candidatecity) VALUES (?, ?, ?, ?, ?)';
+  const q = "SELECT * FROM candidates";
+
+  db.query(q, (err, data) => {
+    if (err) {
+      console.error(err);
+      return; // Don't continue if there's an error
+    }
+    
+    const currentDate = new Date();
+    currentDate.setDate(currentDate.getDate()); // No need to add 1 day here
+    const formattedDate = currentDate.toISOString().slice(0, 10); // Format to 'YYYY-MM-DD'
+    
+    for (let i = 0; i < data.length; i++) {
+      const values = [data[i].id, data[i].name, data[i].votes, formattedDate, data[i].city];
+      
+      const updateQuery = 'UPDATE candidates SET votes = 0 WHERE id = ?';
+      db.query(updateQuery, [data[i].id], (err, result) => {
+        if (err) {
+          console.error(err);
+        }
+        // Don't return a response here
+      });
+
+      db.query(insertQuery, values, (err, result) => {
+        if (err) {
+          console.error(err);
+        }
+      });
+    }
   });
+});
+
 
   module.exports = router;
